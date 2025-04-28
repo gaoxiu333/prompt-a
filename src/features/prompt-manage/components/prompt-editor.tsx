9c6ad7a2b3ef58d03054f11e356f1eb19617632d
xiu <gaoxiu333@gmail.com>
2025-04-29 00:35:25 +0800

    feat: 添加 HSL 转 HEX 工具函数，更新 Monaco 编辑器设置以支持主题颜色动态计算和应用

--- src/features/prompt-manage/components/prompt-editor.tsx
+++ src/features/prompt-manage/components/prompt-editor.tsx
@@ -3,24 +3,37 @@
 import Editor, { Monaco } from '@monaco-editor/react';
 import { MarkerSeverity, Range, Selection, type editor } from 'monaco-editor';
 
+import { useCallback } from 'react';
+
+import { useMonacoSetup } from '@/features/editor/useMonacoSetup';
+
 export function PromptEditor() {
+  const { monacoRef, handleEditorWillMount } = useMonacoSetup({
+    errorFixFn: () => {},
+  });
+
   function handleEditorChange(value, event) {
     // here is the current value
-  }
-
-  function handleEditorDidMount(editor, monaco) {
-    console.log('onMount: the editor instance:', editor);
-    console.log('onMount: the monaco instance:', monaco);
-  }
-
-  function handleEditorWillMount(monaco) {
-    console.log('beforeMount: the monaco instance:', monaco);
   }
 
   function handleEditorValidation(markers) {
     // model markers
     // markers.forEach(marker => console.log('onValidate:', marker.message));
   }
+  const handleEditorDidMount = useCallback(
+    (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+      monacoRef.current = monaco;
+      // editorRef.current = editor;
+
+      // if (autoFocus) {
+      //   moveFocusAtEnd(editor);
+      // }
+
+      // registerActions(editor, monaco);
+      // setIsEditorMounted(true);
+    },
+    [monacoRef],
+  );
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="text-2xl font-bold">Prompt Editor</h1>
