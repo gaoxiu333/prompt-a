16761249ba4901e93211d6eab2870d5f657dcd91
xiu <gaoxiu333@gmail.com>
2025-05-06 15:45:20 +0800

    feat: 重构 LLMProvider 组件，优化布局和样式，移除不必要的 Card 组件

--- src/app/prompts/_components/prompt-editor-layout.tsx
+++ src/app/prompts/_components/prompt-editor-layout.tsx
@@ -6,20 +6,10 @@ import LLMProvider from '@/features/llm/commponents/llm-provider';
 import { PromptEditor } from '@/features/prompt-manage/components/prompt-editor';
 
 const PromptEditorLayout = () => {
-  const [isReady, setIsReady] = useState(false);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsReady(true);
-    }, 1000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
     <div className="p-4 h-full w-full flex flex-col gap-4 items-center justify-start">
       <LLMProvider />
-      <PromptEditor /> 
+      <PromptEditor />
     </div>
   );
 };
--- src/features/llm/commponents/llm-provider.tsx
+++ src/features/llm/commponents/llm-provider.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { Select } from '@radix-ui/react-select';
 import { Edit } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import {
+  Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
@@ -19,54 +18,50 @@ import LLMApiKeyModal from './llm-apikey';
 const LLMProvider = () => {
   const { llmProvider, llmProviderList, handleLLMProviderChange } = useLLM();
   return (
-    <Card className="w-full">
-      <CardContent className="flex flex-row gap-6">
-        <Label>
-          LLM Provider:
-          <Select
-            name="llmProvider"
-            value={llmProvider.id}
-            onValueChange={handleLLMProviderChange}
-          >
-            <SelectTrigger className="w-[180px] cursor-pointer">
-              <SelectValue placeholder="Open AI">
-                {llmProvider.name}
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent className="cursor-pointer">
-              {llmProviderList.map((provider) => (
-                <SelectItem key={provider.id} value={provider.id}>
-                  {provider.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </Label>
-        <Label>
-          Model:
-          <Select name="model">
-            <SelectTrigger className="cursor-pointer w-[200px]">
-              <SelectValue placeholder="请选择" />
-            </SelectTrigger>
-            <SelectContent>
-              {llmProvider.chatModels.map((model) => (
-                <SelectItem key={model.id} value={model.id}>
-                  {model.displayName}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </Label>
-        <Label>
-          API Key: 状态
-          <LLMApiKeyModal>
-            <Button variant={'ghost'}>
-              <Edit style={{ width: '1em', height: '1em' }} />
-            </Button>
-          </LLMApiKeyModal>
-        </Label>
-      </CardContent>
-    </Card>
+    <div className="w-full flex flex-row gap-4">
+      <Label className="flex flex-row gap-4 flex-1">
+        <span className="text-sm text-muted-foreground">LLM Provider:</span>
+        <Select
+          name="llmProvider"
+          value={llmProvider.id}
+          onValueChange={handleLLMProviderChange}
+        >
+          <SelectTrigger className="cursor-pointer flex-1">
+            <SelectValue placeholder="请选择">{llmProvider.name}</SelectValue>
+          </SelectTrigger>
+          <SelectContent className="cursor-pointer">
+            {llmProviderList.map((provider) => (
+              <SelectItem key={provider.id} value={provider.id}>
+                {provider.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </Label>
+      <Label className="flex flex-row gap-4 flex-1">
+        <span className="text-sm text-muted-foreground">Model:</span>
+        <Select name="model">
+          <SelectTrigger className="cursor-pointer flex-1">
+            <SelectValue placeholder="请选择" />
+          </SelectTrigger>
+          <SelectContent>
+            {llmProvider.chatModels.map((model) => (
+              <SelectItem key={model.id} value={model.id}>
+                {model.displayName}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </Label>
+      <Label>
+        API Key: 状态
+        <LLMApiKeyModal>
+          <Button variant={'outline'} className="ml-2 cursor-pointer">
+            <Edit style={{ width: '1em', height: '1em' }} />
+          </Button>
+        </LLMApiKeyModal>
+      </Label>
+    </div>
   );
 };
 
